ee1eb87a66a90bf35dc802a8434a9fe6d60c3708
Julie Adams <julie.ellen.adams@gmail.com>
2020-01-27 20:41:14 -0800

    Make words draggable by element ID

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// import { createSecureContext } from "tls";
+
 console.log("Webpack is working!");
 
 // const words = [];
@@ -8,40 +10,68 @@ console.log("Webpack is working!");
 
 
 window.onload = function() {
+    // async function apiGetAll() {
+    //   try {
+    //     const resp = await fetch(
+    //       "https://cors-anywhere.herokuapp.com/https://api.datamuse.com/words?rel_trg=cow"
+    //     );
+    //     console.log(resp.json());
+    //     return resp.json();
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // }
+
+    // apiGetAll();
+
+    // let words;
+    // fetch(
+    //   "https://cors-anywhere.herokuapp.com/https://api.datamuse.com/words?rel_trg=cow"
+    // )
+    //   .then(response => {
+    //     words = response.json();
+    //     return words;
+    //   })
+    // //   console.log(words)
     
-    const word = document.getElementById("word-1");
+    // let wordId;
+    document.addEventListener("mouseover", e => dragWord(e.target.id));
+    // console.log(wordId)
+
+    const dragWord = wordId => {
+        if (!wordId) return;
+        const word = document.getElementById(wordId);
     
-    word.onmousedown = function(event) {
-        word.style.position = 'absolute';
-        word.style.zIndex = 1000;
-        // document.body.append(word);
-    
-        moveAt(event.pageX, event.pageY);
+        word.onmousedown = function(event) {
+            word.style.position = 'absolute';
+            word.style.zIndex = 1000;
+            // document.body.append(word);
         
-        function moveAt(pageX, pageY) {
-            word.style.left = pageX - word.offsetWidth / 2 + 'px';
-            word.style.top = pageY - word.offsetHeight / 2 + 'px';
+            moveAt(event.pageX, event.pageY);
+            console.log(event.currentTarget.id)
+            
+            function moveAt(pageX, pageY) {
+                word.style.left = pageX - word.offsetWidth / 2 + 'px';
+                word.style.top = pageY - word.offsetHeight / 2 + 'px';
+            }
+        
+            function onMouseMove(event) {
+                moveAt(event.pageX, event.pageY)
+            }
+        
+            document.addEventListener('mousemove', onMouseMove);
+        
+            word.onmouseup = function() {
+                document.removeEventListener('mousemove', onMouseMove);
+                word.onmouseup = null;
+            }
+        }
+        word.ondragstart = function() {
+          return false;
         }
     
-        function onMouseMove(event) {
-            moveAt(event.pageX, event.pageY)
-        }
-    
-        document.addEventListener('mousemove', onMouseMove);
-    
-        word.onmouseup = function() {
-            document.removeEventListener('mousemove', onMouseMove);
-            word.onmouseup = null;
-        }
-    }
-    
-    word.ondragstart = function() {
-      return false;
     };
 
 
-};
 
-function handleDrag(event) {
-    
-}
+};
