467b7ee08d08453a3f3374eba8469df139696a95
Julie Adams <julie.ellen.adams@gmail.com>
2020-03-17 10:17:45 -0700

    Comment displayError function

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -232,7 +232,11 @@ const addWord = (word, idx) => {
     }
     document.getElementById("words").appendChild(wordSpan);
 };
-
+/**
+ * Toggles the visibility of the error alert box and displays the error entered into the parameters
+ * 
+ * @param {String} error The error text to be displayed in the alert box
+ */
 const displayError = error => {
     const alertBox = document.getElementById("alert");
     const alertText= document.getElementById("alert-text");
